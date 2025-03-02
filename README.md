de903a6b0ea52f9f2761cefd327b9edce17ef845
DEEPAK_KUMAR <154451485+Deepakkumarsvg@users.noreply.github.com>
2025-03-02 17:56:47 +0530

    fetch activity on expiry time (#3)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 INSTRUCTION TO BE ADDED
+
+STEP :
+
+1.  git pull origin master
+2.  npm i
+3.  npm run start-dev
--- routes/activity.js
+++ routes/activity.js
@@ -1,12 +1,11 @@
-
 import express from "express";
 import doctorCtrl from "../controllers/doctors.js";
 import activityCtrl from "../controllers/activity.js";
 const activityRoutes = express.Router();
 
-/* GET users listing. */
-activityRoutes.get('/all-activity/:searchQuery?', async (req, res, next) => {
-	activityCtrl.fetchAllActivity(req, res, next)
+/* GET Activity and Doctors listing. */
+activityRoutes.get("/all-activity/:searchQuery?", async (req, res, next) => {
+  activityCtrl.fetchAllActivity(req, res, next);
 });
 
 export default activityRoutes;
